5108e2675731e6d32f9ffc4f3dc407791f290f02
NakuRei <nakurei7901@gmail.com>
2025-05-08 18:26:58 +0900

    feat: update version to 0.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("../../src/"))
 project = "rei_rt_audio"
 copyright = "2025, NakuRei"
 author = "NakuRei"
-release = "0.1.0"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
