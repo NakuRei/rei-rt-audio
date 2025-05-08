0d8c7f5eaa0c423eb83fa3df096a27576f5ad2e6
NakuRei <nakurei7901@gmail.com>
2025-05-08 14:25:22 +0900

    feat: add Sphinx documentation setup and initial files

---
+++ docs/source/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../src/"))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "rei_rt_audio"
+copyright = "2025, NakuRei"
+author = "NakuRei"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+]
+autosummary_generate = True
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "private-members": False,
+    "special-members": False,
+    "inherited-members": False,
+    "show-inheritance": True,
+}
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+language = "en"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
---
+++ docs/source/index.rst
@@ -0,0 +1,24 @@
+.. rei_rt_audio documentation master file, created by
+   sphinx-quickstart on Thu May  8 12:54:46 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+rei_rt_audio documentation
+==========================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+.. autosummary::
+   :toctree: modules
+
+   rei_rt_audio.calculate
+   rei_rt_audio.logger
+   rei_rt_audio.consumer
+   rei_rt_audio.controller
+   rei_rt_audio.dispatcher
+   rei_rt_audio.processor
+   rei_rt_audio.publisher
+   rei_rt_audio.strategy
+   rei_rt_audio.subscriber
